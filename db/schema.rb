e15ec7675edc2feb3097d14d7700772632ca5aaf
nbert100 <nbert14@gmail.com>
2020-01-14 20:22:12 -0500

    add default to visited

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_002759) do
+ActiveRecord::Schema.define(version: 2020_01_15_012119) do
 
   create_table "places", force: :cascade do |t|
     t.string "city"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_01_15_002759) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "visited", default: false
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
